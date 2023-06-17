8f9571f92c3967a28d1651091826be23cd69e88d
MorphTail <chitniskshitij@gmail.com>
2023-06-17 08:47:24 +0530

    ga

--- build.sh
+++ build.sh
@@ -6,3 +6,21 @@ rm -r -d */
 rm -rf index.html
 unzip *.zip
 mv *.html index.html
+html_file=index.html
+ga_tag="<script async src="https://www.googletagmanager.com/gtag/js?id=G-7Q2WC782QM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7Q2WC782QM');
+</script>"
+
+# ed -s "$html_file" <<EOF
+# /<body>/i
+# $ga_tag
+# .
+# w
+# q
+# EOF
+
